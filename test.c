2c2122fc7a901e88b67aba7c62d0070492720144
p96114175 <p96114175@github.com>
2023-05-07 23:54:58 +0800

    Fix typo in introduction to user guide

---
+++ test.c
@@ -0,0 +1,42 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define FIB_DEV "/dev/fibonacci"
+
+long long sz, start, real_time, k_time;
+
+static inline long long get_ns()
+{
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    return t.tv_nsec + t.tv_sec;
+}
+int main()
+{
+    char buf[1];
+    char write_buf[] = "testing writing";
+    int offset = 92;
+
+    int fd = open(FIB_DEV, O_RDWR);
+
+    if (fd < 0) {
+        perror("Failed to open character device");
+        exit(1);
+    }
+    write(fd, write_buf, strlen(write_buf));
+    for (int i = 0; i <= offset; i++) {
+        lseek(fd, i, SEEK_SET);
+        start = get_ns();
+        sz = read(fd, buf, 1);
+        real_time = get_ns() - start;
+        k_time = write(fd, write_buf, strlen(write_buf));
+        printf("%d %lld %lld %lld\n", i, real_time, k_time, real_time - k_time);
+    }
+    close(fd);
+    return 0;
+}
